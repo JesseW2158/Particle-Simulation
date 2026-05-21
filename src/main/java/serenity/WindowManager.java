73a66b4f70538e3d2910755cdca2fbd2f417710a
Jesse Wang <jessewang2158@gmail.com>
2026-05-21 12:37:53 -0400

    G

---
+++ src/main/java/serenity/WindowManager.java
@@ -0,0 +1,165 @@
+package serenity;
+
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.system.MemoryUtil;
+
+public class WindowManager {
+    public static final float FOV = (float) Math.toRadians(60);
+    public static final float Z_NEAR = 0.01f;
+    public static final float Z_FAR = 1000f;
+
+    private final String title;
+
+    private int width, height;
+    private long windowHandle;
+
+    private boolean resize, vSync;
+
+    private final Matrix4f projectionMatrix;
+
+    public WindowManager(String title, int width, int height, boolean vSync) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+        this.vSync = vSync;
+        this.projectionMatrix = new Matrix4f();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!GLFW.glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        GLFW.glfwDefaultWindowHints();
+        GLFW.glfwWindowHint(GLFW.GLFW_VISIBLE, GLFW.GLFW_FALSE);
+        GLFW.glfwWindowHint(GLFW.GLFW_RESIZABLE, GLFW.GLFW_TRUE);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MAJOR, 3);
+        GLFW.glfwWindowHint(GLFW.GLFW_CONTEXT_VERSION_MINOR, 2);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_PROFILE, GLFW.GLFW_OPENGL_CORE_PROFILE);
+        GLFW.glfwWindowHint(GLFW.GLFW_OPENGL_FORWARD_COMPAT, GLFW.GLFW_TRUE);
+
+        boolean maximized = false;
+
+        if (width == 0 || height == 0) {
+            width = 100;
+            height = 100;
+            GLFW.glfwWindowHint(GLFW.GLFW_MAXIMIZED, GLFW.GLFW_TRUE);
+            maximized = true;
+        }
+
+        windowHandle = GLFW.glfwCreateWindow(width, height, title, MemoryUtil.NULL, MemoryUtil.NULL);
+
+        if (windowHandle == MemoryUtil.NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        GLFW.glfwSetFramebufferSizeCallback(windowHandle, (window, newWidth, newHeight) -> {
+            this.width = newWidth;
+            this.height = newHeight;
+            this.resize = true;
+        });
+
+        GLFW.glfwSetKeyCallback(windowHandle, (windowHandle, key, scancode, action, mods) -> {
+            if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_RELEASE) {
+                GLFW.glfwSetWindowShouldClose(windowHandle, true);
+            }
+        });
+
+        if (maximized) {
+            GLFW.glfwMaximizeWindow(windowHandle);
+        } else {
+            GLFWVidMode vidMode = GLFW.glfwGetVideoMode(GLFW.glfwGetPrimaryMonitor());
+            GLFW.glfwSetWindowPos(windowHandle, (vidMode.width() - width) / 2, (vidMode.height() - height) / 2);
+        }
+
+        GLFW.glfwMakeContextCurrent(windowHandle);
+
+        if(vSync) {
+            GLFW.glfwSwapInterval(1);
+        }
+
+        GLFW.glfwShowWindow(windowHandle);
+
+        GL.createCapabilities();
+
+        GL11.glClearColor(0f, 0f, 0f, 0f);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_STENCIL_TEST);
+        GL11.glEnable(GL11.GL_CULL_FACE);
+        GL11.glCullFace(GL11.GL_BACK);
+    }
+
+    public void update() {
+        GLFW.glfwSwapBuffers(windowHandle);
+        GLFW.glfwPollEvents(); 
+    }
+
+    public void cleanup() {
+        GLFW.glfwDestroyWindow(windowHandle);
+    }
+
+    public void setClearColor(float r, float g, float b, float a) {
+        GL11.glClearColor(r, g, b, a);
+    }
+
+    public boolean windowShouldClose() {
+        return GLFW.glfwWindowShouldClose(windowHandle);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        GLFW.glfwSetWindowTitle(windowHandle, title);
+    }
+
+    public boolean isvSync() {
+        return vSync;
+    }
+
+    public void setVSync(boolean vSync) {
+        this.vSync = vSync;
+    }
+
+    public boolean isResize() {
+        return resize;
+    }
+
+    public void setResize(boolean resize) {
+        this.resize = resize;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public long getWindowHandle() {
+        return windowHandle;
+    }
+
+    public Matrix4f getProjectionMatrix() {
+        return projectionMatrix;
+    }
+
+    public Matrix4f updateProjectionMatrix() {
+        float aspectRatio = (float) width / height;
+        return projectionMatrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+    }
+
+    public Matrix4f updateProjectMatrix(Matrix4f matrix, int width, int height) {
+        float aspectRatio = (float) width / height;
+        return matrix.setPerspective(FOV, aspectRatio, Z_NEAR, Z_FAR);
+    }
+}
